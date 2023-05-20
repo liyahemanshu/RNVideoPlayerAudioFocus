faf8195b3e1b86ce94006faed107a022aa15a443
Hemanshu Liya <hemanshuliya@gmail.com>
2023-05-20 17:34:58 +0530

    React Native Video Patch added for Audio Focus

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import Video from 'react-native-video';
+import video from './src/broadchurch.mp4';
+import React from 'react';
+import {View, Button} from 'react-native';
+
+export default App = () => {
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [isMuted, setIsMuted] = React.useState(false);
+
+  // const togglePlaying = () => {};
+
+  return (
+    <View>
+      <Video
+        source={video}
+        paused={!isPlaying}
+        controls={true}
+        style={{height: 200, width:200}}
+        muted={isMuted}
+        gainAudioFocus={isMuted ? false: true}
+      />
+      <Button
+        onPress={() => setIsPlaying(p => !p)}
+        title={isPlaying ? 'Stop' : 'Play'}
+      />
+      <Button
+        onPress={() => setIsMuted(m => !m)}
+        title={isMuted ? 'Unmute' : 'Mute'}
+      />
+    </View>
+  );
+};
